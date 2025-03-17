cce87a74c0a57855d0456ee5d3e9e9e66eda9388
Othmane Bouizi <obouizi@e2r6p2.1337.ma>
2025-03-17 14:29:51 +0000

    add files

---
+++ ft_malloc.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouizi <obouizi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 04:16:21 by obouizi           #+#    #+#             */
+/*   Updated: 2025/03/17 14:05:40 by obouizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
+
+# include <stdlib.h>
+
+typedef struct s_garbage_collector
+{
+	void						*ptr;
+	struct s_garbage_collector	*next;
+}								t_grbg_collector;
+
+void	*ft_malloc(size_t size);
+void	free_garbage(void);
+
+#endif
